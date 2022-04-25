2d4279381ad2bd7625a79dbfba1467c9c9c2be0f
devmustroc <elmustapha.abourar@outlook.com>
2022-04-25 18:02:00 +0200

    Check Cycled Singly LinkedList

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - fucntion that check if a S LinkedList has a cycle in it.
+ * @list: pointer to the beginning of the node.
+ * Return: 0 if no cycle, 1 if there is a cycle.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *cursor;
+	listint_t *head;
+
+	head = list;
+	cursor = list;
+	while (cursor != NULL && cursor->next != NULL)
+	{
+		head = head->next;
+		cursor = cursor->next->next;
+		if (head == cursor)
+			return (1);
+	}
+	return (0);
+}
