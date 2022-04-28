fad261fcb96cf1411a3495cc49d36b21f9ebed5d
devmustroc <elmustapha.abourar@outlook.com>
2022-04-28 17:03:23 +0200

    Calculator ARGV

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
+
     op = argv[2]
     num1 = int(argv[1])
     num2 = int(argv[3])
 
     if op is '+':
-        print("{:d} + {:d} = {:d}".format(num1, num2, add(nu1, num2)))
+        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
     elif op is '-':
         print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
     elif op is '*':
