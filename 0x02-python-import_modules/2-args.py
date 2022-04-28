84ccac9de119472ea4402451ce2e3a0d50fb69af
devmustroc <elmustapha.abourar@outlook.com>
2022-04-28 12:42:18 +0200

    len argements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,12 @@
-#!/bin/bash
-from sys import argv
-
-
+#!/usr/bin/python3
 if __name__ == "__main__":
     import sys
     args = len(sys.argv) - 1
-    if args is 0:
+    if args == 0:
         print("{:d} argements".format(args))
-    elif args is 1:
+    elif args == 1:
         print("{:d} argement:".format(args))
     else:
         print("{:d} argements".format(args))
-    for i in range (1, len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
