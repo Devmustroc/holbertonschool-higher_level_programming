5b7c987a414aaa480701b5482e116d5715fb23c7
devmustroc <elmustapha.abourar@outlook.com>
2022-04-29 21:01:05 +0200

    bytecode manip

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if (a < b):
-        return (c)
-    elif (c > b):
-        return (a + b)
+def magic_calculation(a, b):
+    from 102_magic_calculation import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
     else:
-        return ((a * b) - c)
+        return sub(a, b)
