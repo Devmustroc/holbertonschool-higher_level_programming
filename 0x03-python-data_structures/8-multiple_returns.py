c468948f978e98f8ee5a17712df48103d7fee92c
devmustroc <elmustapha.abourar@outlook.com>
2022-05-02 15:51:09 +0200

    Mult return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from functools import total_ordering
 
-
+#!/usr/bin/python3
 def multiple_returns(sentence):
     total = len(sentence)
     char = sentence[0] if total > 0 else "None"
