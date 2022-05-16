222d06f41d7a6b593136dd76f544c47cc74d58ed
devmustroc <elmustapha.abourar@outlook.com>
2022-05-16 12:38:20 +0200

    handling Error for integer

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     i = 0;
     try:
-        for items in range(0, x):
+        for items in range(x):
             print("{:d}".format(my_list[items]), end="")
             i += 1
     except (ValueError, TypeError):
-        pass
+            pass
 
     print()
     return i
