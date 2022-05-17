ecef474887853a5bab81bb7fc34b6ba2c5211db2
devmustroc <elmustapha.abourar@outlook.com>
2022-05-17 17:47:14 +0200

    Comments adding standart

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,13 +8,11 @@ class square():
 
     def __init__(self, size=0):
         """
-
         Args:
             size (int, size): size of square
         """
         if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be <= 0")
-        else:
-            self.__size = size
+        self.__size = size
