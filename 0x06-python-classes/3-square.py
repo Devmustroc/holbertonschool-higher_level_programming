466115dd70da0fbef1c63ea010a32512033ba933
devmustroc <elmustapha.abourar@outlook.com>
2022-05-17 18:22:39 +0200

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,9 +6,10 @@ class Square():
     """
         Square class with private instance attribute size
     """
+
+
     def __init__(self, size=0):
         """
-
         Args:
             size : size of the square
 
