7fe8d688d973c6ac63656cff9d1f923214a3442b
devmustroc <elmustapha.abourar@outlook.com>
2022-05-17 18:05:12 +0200

    Comments adding standart

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-class Square():
-    '''
-        Defining a Square
-    '''
+"""
+Square class definition
+"""
+
+
+class Square:
+    """
+    Square class with private instance attribute size
+    """
 
     def __init__(self, size=0):
-        '''Initialization of instance attributes
-            Args:
-            size (int): Zero or positve number.
-        '''
-        if not isinstance(size, int):
+        """
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
