86a00e7115b9e6d14600e5e0e1fa5dd48aa7ff84
devmustroc <elmustapha.abourar@outlook.com>
2022-05-23 21:21:04 +0200

    edit symboles

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -77,8 +77,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         for row in range(self.__height - 1):
-            print("#" * self.__width)
-        return ('#' * self.__width)
+            print(str(self.print_symbol) * self.__width)
+        return str(str(self.print_symbol) * self.__width)
 
     def __repr__(self):
         """repr method"""
