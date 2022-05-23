ba3c7ae059a23acf0af627818cd08ec29643ae43
devmustroc <elmustapha.abourar@outlook.com>
2022-05-23 21:11:46 +0200

    Project checks

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -53,6 +53,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """method to return area of width and height"""
         return self.__width * self.__height
