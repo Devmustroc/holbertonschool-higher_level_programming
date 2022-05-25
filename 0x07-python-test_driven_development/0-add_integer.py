ad41022404eb1068eb4776ed494bad35adb3371e
devmustroc <elmustapha.abourar@outlook.com>
2022-05-25 09:26:37 +0200

    recheck

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-This module adds two integers and returns sum of two numbers a & b.
-Only accepts integers and floats else TypeError is raised
-This module will convert float to integer
+Module to sum
+add_integer: adds two integers and return
 """
 
 
 def add_integer(a, b=98):
+    """Return sum of a + b
+    Args: a = int/float b = int/float
     """
-    add_integer: Check input if correct, cast both into ints and return sum
-    """
+
     if isinstance(a, (int, float)) is False:
         raise TypeError('a must be an integer')
-    elif isinstance(b, (int, float)) is False:
+    if isinstance(b, (int, float)) is False:
         raise TypeError('b must be an integer')
-    return(int(a) + int(b))
+    return int(a + b)
