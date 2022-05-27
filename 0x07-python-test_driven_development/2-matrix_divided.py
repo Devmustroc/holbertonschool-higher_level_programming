1be34102270b66c573889da9593bcdad6d32541f
devmustroc <elmustapha.abourar@outlook.com>
2022-05-27 13:18:57 +0200

    new Edit

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
-'''
-    Divides matrix by a divisor
-    matrix (list)
-    div (division factor)
-'''
+"""
+Matrix Manipulation
+"""
 
 
 def matrix_divided(matrix, div):
-    '''
-        divides a matrix value by a divisor
-    '''
-    new_matrix = []
-    try:
-        length = len(matrix[0])
-    except:
-        pass
-    int_err = "matrix must be a matrix (list of lists) of integers/floats"
+    """Divide all elements of the matrix
+    Parameters:
+    matrix = matrix
+    div = integer dividing elements
+    Local Args:
+    new = list of lists
+    array = lists with new elements
+    """
+    mes0 = "matrix must be a matrix (list of lists) of integers/floats"
+    mes1 = "Each row of the matrix must have the same size"
+    res_matrix = []
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    for row in matrix:
-        new_row = []
-        if not isinstance(row, list):
-            raise TypeError(int_err)
-        if length != len(row):
-            raise TypeError("Each row of the matrix must have the same size")
-        for l in row:
-            if not isinstance(l, (int, float)):
-                raise TypeError(int_err)
-            result = round(l / div, 2)
-            new_row.append(result)
-        new_matrix.append(new_row)
-    return new_matrix
+    for lists in matrix:
+        if len(lists) != len(matrix[0]):
+            raise TypeError(mes1)
+        inner_list = []
+        for items in lists:
+            if not isinstance(items, (int, float)):
+                raise TypeError(mes0)
+            else:
+                inner_list.append(round(items / div, 2))
+        res_matrix.append(inner_list)
+
+    return res_matrix
