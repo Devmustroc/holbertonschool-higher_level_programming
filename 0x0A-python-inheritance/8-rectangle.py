e676c2fd51140c85043141282266daa20ec30ee5
devmustroc <elmustapha.abourar@outlook.com>
2022-05-30 18:26:45 +0200

    Inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,7 +5,7 @@ Class Module
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class rectangle(BaseGeometry):
+class Rectangle(BaseGeometry):
     """Rectangle Subclass"""
     def __init__(self, width, height):
         """initialization method"""
