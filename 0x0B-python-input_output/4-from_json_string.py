b765998f4e140ba32da89d422f30a08a25181f82
DevMust <elmustapha.abourar@outlook.com>
2022-05-31 13:16:09 +0200

    Json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" File name : 4-from_json_string.py"""
+import json
+
+
+def to_json_string(my_obj):
+    """from_json_string  returns an object
+    (python data struct) respresented by a JSON string
+
+    Args:
+    my_obj (obj): anything object e.g: list dict
+    """
+    return(json.loads(my_obj))
