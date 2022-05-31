639547586a2d1baefc05d44d6bebf2e060aec75d
DevMust <elmustapha.abourar@outlook.com>
2022-05-31 19:30:26 +0200

    I/O Student part 2

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,21 +7,23 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Special method to intialize"""
-        self.first_name = first_name
-        self.last_name = last_name
         self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
 
     def to_json(self, attrs=None):
         """Method that returns directory description"""
         obj = self.__dict__.copy()
-        for item in attrs:
-            if type(item) != str:
-                return obj
+        if type(attrs) == list:
+            for item in attrs:
+                if type(item) != str:
+                    return obj
 
-        d_list = {}
+                d_list = {}
 
-        for atr in range(len(attrs)):
-            for satr in obj:
-                if attrs[atr] == satr:
-                    d_list[satr] = obj[satr]
-            return obj
+                for atr in range(len(attrs)):
+                    for satr in obj:
+                        if attrs[atr] == satr:
+                            d_list[satr] = obj[satr]
+                return d_list
+        return obj
