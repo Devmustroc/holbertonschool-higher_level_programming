d0f3050e971d0db81d33c1ee1bff299ba16efcee
DevMust <elmustapha.abourar@outlook.com>
2022-05-31 21:28:16 +0200

    I/O Student 2

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -7,8 +7,8 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Special method to intialize"""
-        self.first_name = first_name
         self.last_name = last_name
+        self.first_name = first_name
         self.age = age
 
     def to_json(self, attrs=None):
@@ -16,12 +16,12 @@ class Student:
         if attrs is None:
             return self.__dict__
         ret_att = {}
-        for key, val in self.__dict__.items():
+        for key, value in self.__dict__.items():
             if key in attrs:
                 ret_att[key] = value
         return ret_att
 
-    def reload_form_json(self, json):
-        """Replaces all attributes of the student instance"""
-        for key, value in json.items():
-            setattr(self, key, value)
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance """
+        for atr in json:
+            self.__dict__[atr] = json[atr]
