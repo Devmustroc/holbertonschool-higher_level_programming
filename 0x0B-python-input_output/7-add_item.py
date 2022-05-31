0f116260ed55100110140dc7aea2e0c640ff6a86
DevMust <elmustapha.abourar@outlook.com>
2022-05-31 16:47:52 +0200

    I/O add items

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,8 +9,9 @@ load_file = __import__('6-load_from_json_file').load_from_json_file
 save_file = __import__('5-save_to_json_file').save_to_json_file
 
 my_list = []
-if os.path.("add_item.json"):
+if os.path.exists("add_item.json"):
     my_list = load_file("add_item.json")
+
 for arg in sys.argv[1:]:
     my_list.append(arg)
 
