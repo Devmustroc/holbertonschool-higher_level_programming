1d7c6fb48543de7ecb3c2cc4340bb6a02e57d519
DevMust <elmustapha.abourar@outlook.com>
2022-05-31 21:03:55 +0200

    I/O Student 2

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -7,19 +7,19 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Special method to intialize"""
-        self.age = age
-        self.last_name = last_name
         self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
         """Method that returns directory description"""
         if attrs is None:
             return self.__dict__
-        re_att = {}
+        ret_att = {}
         for key, val in self.__dict__.items():
             if key in attrs:
-                re_att[key] = val
-        return re_att
+                ret_att[key] = value
+        return ret_att
 
     def reload_form_json(self, json):
         """Replaces all attributes of the student instance"""
