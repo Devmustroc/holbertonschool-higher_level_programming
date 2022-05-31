530d5f8e7162eb245f239f245813d53f49aae9d8
DevMust <elmustapha.abourar@outlook.com>
2022-05-31 12:59:18 +0200

    Edit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""file name : write_file.py"""
+
+def write_file(filename="", text=""):
+    """write_file: write a string
+    to a text file (utf-8)
+
+    Args:
+        filename(str): default.
+        text(str): text to add. Default
+    """
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
+
