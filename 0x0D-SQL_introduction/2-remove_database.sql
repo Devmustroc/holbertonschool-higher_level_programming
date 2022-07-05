4c6054c56e04c45cd409727d201162d3efd837be
devmustroc <@Ismail251217>
2022-07-05 13:22:43 +0200

    mysql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,2 @@
---ADD DATABASE.
---If the database hbtn_0c_0 doesn’t exist
-DROP DATABASE IF NOT EXISTS hbtn_0c_0
-                                      
+-- Remove DataBase If the database hbtn_0c_0 doesn’t exist
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;                                    
