d488af05dde7b731009b5b0445ef6d98ef71686e
devmustroc <@Ismail251217>
2022-07-06 20:58:42 +0200

    Database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,9 +1,2 @@
 -- creates the database hbtn_0c_0 in your MySQL server.
-CREATE USER
-IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
