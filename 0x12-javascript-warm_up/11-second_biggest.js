a94334f0e25e69f413bd99e749d3612f1da13d77
devmustroc <@Ismail251217>
2022-08-01 20:25:58 +0200

    Secound test

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-let bigg = 0;
-const args = process.argv.slice(2);
-if (args.length > 1) {
-  args.sort();
-  bigg = args[args.length - 2];
+const Numbers = process.argv.length;
+const numTest = process.argv.slice(2);
+if (Numbers < 4) {
+  console.log(0);
+} else {
+  const CheckTest = numTest.sort((a, b) => b - a);
+  console.log(CheckTest[1]);
 }
-console.log(bigg);
