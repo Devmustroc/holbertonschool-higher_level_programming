4a7977045aa1f9b32c008a5785394903d708f906
devmustroc <@Ismail251217>
2022-08-07 12:04:51 +0200

    New

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+script that lists all states from the database hbtn_0e_0_usa:
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=argv[1],
+                                 passwd=argv[2],
+                                 db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for states in cursor.fetchall():
+        print(states)
+    cursor.close()
+    connection.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa: 
+"""
+
+from multiprocessing import connection
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=argv[1],
+                                 passwd=argv[2],
+                                 db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+                    ORDER BY id ASC")
+    for states in cursor.fetchall():
+        print(states)
+    cursor.close()
+    connection.close()
