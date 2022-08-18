bc22381339c4ae281c0e28f5d04d703b58ff00ee
devmustroc <@Ismail251217>
2022-08-18 14:09:52 +0200

    wc & NewLine

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays
-# all HTTP methods the server will accept
+# takes a URL & displays all HTTP methods the server will accept
 curl -sI "$1" | grep "Allow:" | cut -d " " -f1-
