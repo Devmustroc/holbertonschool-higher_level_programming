7bf615baa1605ddf5fdeecb76aa23a985f84805c
devmustroc <@Ismail251217>
2022-08-19 11:20:37 +0200

    script fetch

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Python script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request as req
+
+if __name__ == "__main__":
+    with req.urlopen("https://intranet.hbtn.io/status") as result:
+        html = result.read()
+        print(f"Body response")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf-8 content: {html.decode('utf-8')}")
