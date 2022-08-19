5f5ee43fa49895e9f5b8dca40da4427db00abc5d
devmustroc <@Ismail251217>
2022-08-19 12:24:49 +0200

    script fetch & sending email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    takes in a URL and an email, sends a POST request to the passed URL with the
+    email as a parameter, and displays the body of the response
+"""
+import urllib.request as res
+import urllib.parse as ps
+from sys import argv
+
+if __name__ == "__main__":
+    email = argv[2]
+    pData = ps.urlencode({'email': email}).encode('utf-8')
+    link = res.Request(argv[1], pData)
+    with res.urlopen(link) as result:
+        print(result.read().decode('utf-8'))
