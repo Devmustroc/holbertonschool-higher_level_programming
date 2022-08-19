45f863c4bc7966517d0db00e637fb0f8ae585c50
devmustroc <@Ismail251217>
2022-08-19 13:34:11 +0200

    Recheck

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-    Python script that fetches https://intranet.hbtn.io/status
+script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request as req
 
 if __name__ == "__main__":
-    with req.urlopen("https://intranet.hbtn.io/status") as result:
-        html = result.read()
-        print(f"Body response")
+    with req.urlopen('https://intranet.hbtn.io/status') as res:
+        html = res.read()
+        print("Body response:")
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
         print(f"\t- utf-8 content: {html.decode('utf-8')}")
