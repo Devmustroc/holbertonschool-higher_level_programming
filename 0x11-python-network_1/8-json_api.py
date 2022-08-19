509983536118dd2f92a13e5baa2d5cf75ead4f6d
devmustroc <@Ismail251217>
2022-08-19 20:16:32 +0200

    Append in JSON

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(dic_resu.get('id'), dic_resu.get('name')))
-    except:
+    except TypeError:
         print("Not a valid JSON")
