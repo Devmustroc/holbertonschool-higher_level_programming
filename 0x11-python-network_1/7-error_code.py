11bb61d09d48a01176abb1b04bbae049a81e9e3e
devmustroc <@Ismail251217>
2022-08-19 15:43:54 +0200

    fetch argv Link

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,13 +2,12 @@
 """
 takes in a URL, sends a request to the URL and displays the body of response
 """
-
-import requests
+import requests as req
 from sys import argv
 
 if __name__ == "__main__":
-    link = requests.get(argv[1])
-    if link.status_code >= 400:
-        print("Error code: {}".format(link.status_code))
+    lk = req.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(lk.status_code))
     else:
-        print(link.text)
+        print(lk.text)
