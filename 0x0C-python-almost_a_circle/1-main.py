f5743b0285e95c1785a3cebab96b9c187fd455d9
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-01-31 17:08:27 +0100

    Recheck

--- 0x0C-python-almost_a_circle/1-main.py
+++ 0x0C-python-almost_a_circle/1-main.py
@@ -4,11 +4,11 @@ from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 2)
+    r1 = Rectangle(10, 2) # create an instance of class Rectangle
     print(r1.id)
 
-    r2 = Rectangle(2, 10)
+    r2 = Rectangle(2, 10)   # create an instance of class Rectangle
     print(r2.id)
 
-    r3 = Rectangle(10, 2, 0, 0, 12)
+    r3 = Rectangle(10, 2, 0, 0, 12)  # create an instance of class Rectangle
     print(r3.id)
--- 0x0C-python-almost_a_circle/2-main.py
+++ 0x0C-python-almost_a_circle/2-main.py
@@ -2,26 +2,26 @@
 """ 2-main """
 from models.rectangle import Rectangle
 
-if __name__ == "__main__":
+if __name__ == "__main__": # if the file is executed as the main program
 
-    try:
-        Rectangle(10, "2")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    try: # try to execute the following code
+        Rectangle(10, "2") # create an instance of class Rectangle
+    except Exception as e: # if an exception is raised
+        print("[{}] {}".format(e.__class__.__name__, e)) # print the exception
 
-    try:
-        r = Rectangle(10, 2)
-        r.width = -10
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    try: # try to execute the following code
+        r = Rectangle(10, 2)  # create an instance of class Rectangle
+        r.width = -10  # assign the private instance attribute
+    except Exception as e:  # if an exception is raised
+        print("[{}] {}".format(e.__class__.__name__, e))  # print the exception
 
-    try:
-        r = Rectangle(10, 2)
-        r.x = {}
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    try:  # try to execute the following code
+        r = Rectangle(10, 2)  # create an instance of class Rectangle
+        r.x = {}  # assign the private instance attribute
+    except Exception as e:  # if an exception is raised
+        print("[{}] {}".format(e.__class__.__name__, e))  # print the exception
 
-    try:
-        Rectangle(10, 2, 3, -1)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    try:  # try to execute the following code
+        Rectangle(10, 2, 3, -1)  # create an instance of class Rectangle
+    except Exception as e:  # if an exception is raised
+        print("[{}] {}".format(e.__class__.__name__, e))  # print the exception
