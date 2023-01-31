bbf77de97c4d9e430c3863bb7539d8cef6c2e993
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-01-31 17:07:28 +0100

    Recheck

--- 0x0C-python-almost_a_circle/0-main.py
+++ 0x0C-python-almost_a_circle/0-main.py
@@ -2,19 +2,19 @@
 """ 0-main """
 from models.base import Base # import class Base
 
-if __name__ == "__main__": # if the file is executed as the main program
+if __name__ == "__main__":  # if the file is executed as the main program
 
-    b1 = Base()
+    b1 = Base()  # create an instance of class Base
     print(b1.id)
 
-    b2 = Base()
+    b2 = Base()  # create an instance of class Base
     print(b2.id)
 
-    b3 = Base()
+    b3 = Base() # create an instance of class Base
     print(b3.id)
 
-    b4 = Base(12)
+    b4 = Base(12) # create an instance of class Base
     print(b4.id)
 
-    b5 = Base()
+    b5 = Base() # create an instance of class Base
     print(b5.id)
